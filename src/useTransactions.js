10a59b39149ddf86c6fdbf3802fb1a581fe61f30
sudoallenlopes23 <allenlopes10@gmail.com>
2023-04-22 19:20:16 +0530

    useTransactions.js(hook) file edited with Details.jsx file

--- src/useTransactions.js
+++ src/useTransactions.js
@@ -17,6 +17,22 @@ const useTransactions = (title) => {
 
     transactionsOfOneSpecificType.forEact((t) => {
         const category = categories.find((c) => c.type === t.category)
-    })
+
+        if(category) category.amount += t.amount;
+    });
+
+    const filteredCategories = categories.filter((c) => c.amount > 0);
+
+    const chartData = {
+        datasets: [{
+            data: filteredCategories.map((c) => c.amount),
+            backgroundColor: filteredCategories.map((c) => c.color)
+        }],
+        labels: filteredCategories.map((c) => c.type)
+    }
+
+    return { total, chartData }
 }
+
+export default useTransactions;
 
