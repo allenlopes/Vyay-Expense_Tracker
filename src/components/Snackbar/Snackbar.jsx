a326010c0f6a14c615088c99bc3c1a98f3cb8088
sudoallenlopes23 <allenlopes10@gmail.com>
2023-04-28 01:40:28 +0530

    Snackbar.jsx and styles.jsx file edited in SnackBar Folder

---
+++ src/components/Snackbar/Snackbar.jsx
@@ -0,0 +1,33 @@
+// Basically Snackbar.jsx is the pop up message when an user fills the form of income and then this pop is shown.
+import React from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+import useStyles from './styles';
+
+const CustomizedSnackbar = ({ open, setOpen }) => {
+    const classes = useStyles();
+
+    const handleClose = (event, reason) => {
+        if(reason === 'clickaway') return;
+
+        setOpen(false);
+    }
+
+    return (
+    <div className={classes.root}>
+        <Snackbar
+            anchorOrigin={{ vertical: 'top', horizontal: 'right'}}
+            open={open}
+            autoHideDuration={3000}
+            onClose={handleClose}
+        >
+            <MuiAlert onClose={handleClose} severity="success" elevation={6} variant='filled'>
+                Transaction Successfully Created.
+            </MuiAlert>
+        </Snackbar>
+    </div>
+  )
+}
+
+export default Snackbar
