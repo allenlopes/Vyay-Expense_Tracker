d1af24a4f1923d1e5c87b54ebccbe4d8f605f102
sudoallenlopes23 <allenlopes10@gmail.com>
2023-04-27 11:54:48 +0530

    Main.jsx file modified

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,11 @@
-import React from 'react';
-import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+import React, { useContext } from 'react';
+import { Card }from '@material-ui/core';
+import { CardHeader } from '@material-ui/core';
+import { CardContent } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import { Divider } from '@material-ui/core';
+import { ExpenseTrackerContext } from '../../context/context';
 
 import useStyles from './styles';
 import Form from './Form/Form';
@@ -7,12 +13,13 @@ import List from './List/List';
 
 const Main = () => {
     const classes = useStyles();
+    const { balance } = useContext(ExpenseTrackerContext);
 
   return (
     <Card className={classes.root} >
         <CardHeader title="Expense Tracker" subheader="- By Allen Lopes" />
         <CardContent>
-            <Typography align="center" variant="h5">Total Balance <span>&#8377;</span>100</Typography>
+            <Typography align="center" variant="h5">Total Balance <span>&#8377;</span>{balance}</Typography>
             <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
                 {/* InfoCard..*/}
                 Try Saying: Add income for <span>&#8377;</span>100 in category Salary for Monday....
