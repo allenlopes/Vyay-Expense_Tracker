24f75586ddc148cdcab7fcaceaf23034cf1a57cd
sudoallenlopes23 <allenlopes10@gmail.com>
2023-04-27 11:25:36 +0530

    context.js file modified

--- src/context/context.js
+++ src/context/context.js
@@ -2,7 +2,7 @@ import React, { useReducer, createContext } from 'react';
 
 import contextReducer from './contextReducer';
 
-const initialState = [];
+const initialState = [{ amount: 5, category: 'Savings', type: 'Income', date: '27-04-2023', id: '1' }];
 
 export const ExpenseTrackerContext = createContext(initialState);
 
