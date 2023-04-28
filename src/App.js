cbf66ea0f2ef0de0cd3cbc41052b369e4929afc9
sudoallenlopes23 <allenlopes10@gmail.com>
2023-04-28 23:41:04 +0530

    App.js and styles.js was edited

--- src/App.js
+++ src/App.js
@@ -12,21 +12,21 @@ const App = () => {
     const main = useRef(null)
   return (
     <div>
-        <p>This is a para </p>
         <Grid className={classes.grid} container spacing={0} alignItems="center"  justifyContent='center' style={{ height: '100vh' }}>
-            <Grid item xs={12} sm={4}>
+
+            <Grid item xs={12} sm={4} className={classes.mobile}>
                 <Details title="Income" />
             </Grid>
 
-            <Grid ref={main} item xs={12} sm={3}>
+            <Grid ref={main} item xs={12} sm={3} className={classes.main}>
                 <Main />
             </Grid>
 
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} sm={4} className={classes.desktop}>
                 <Details title="Income" />
             </Grid>
 
-            <Grid item xs={12} sm={4}>
+            <Grid item xs={12} sm={4} className={classes.last}>
                 <Details title="Expense" />
             </Grid>
 
